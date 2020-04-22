974d9de96d172d4baad305f22b4e334036328aa2
Your Name <you@example.com>
2020-04-22 16:08:17 +0530

    utility

--- Assignment/codes/ee18btech11002/polarplot.py
+++ Assignment/codes/ee18btech11002/polarplot.py
@@ -1,12 +1,15 @@
 # Code by Abhishek Shetkar
-# 16/04/2020
+# 22/04/2020
 # Released under GNU GPL
 import numpy as np
 import matplotlib.pyplot as plt
 import control
 
-w = np.linspace(-20,20,2001) #ranging omega
+w = np.append(np.linspace(0,0.1,1000),np.linspace(0.1,20,1000)) #ranging omega
 
+unici = np.linspace(0 ,2*np.pi, 2000)
+val = np.ones((2000,))
+#phi = -PHASE.reshape((2001,))[idx]
 #G(s)
 s = control.TransferFunction.s
 G = 1/((s)*(1+s)*(1+s))                                       
@@ -17,8 +20,9 @@ Mag, Ph, W = G.freqresp(w)
 
 ax = plt.subplot(111, projection='polar')
 
-ax.plot(Ph.reshape((2001,))[-995:-800],Mag.reshape((2001,))[-995:-800])
-
+ax.plot(Ph.reshape((2000,))[-995:-800],Mag.reshape((2000,))[-995:-800])
+ax.plot(unici.reshape((2000,)), val) 
+#ax.plot(phi, 1, 'o', label = '$\omega_{gc}$' )
 plt.show()
 
 
@@ -33,6 +37,6 @@ iMag, iPh, iW = iG.freqresp(w)
 
 ax = plt.subplot(111, projection='polar')
 
-ax.plot(iPh.reshape((2001,))[-995:-800],iMag.reshape((2001,))[-995:-800])
+ax.plot(iPh.reshape((2000,))[-995:-800],iMag.reshape((2000,))[-995:-800])
 
 plt.show()
